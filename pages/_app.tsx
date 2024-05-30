3848e67892ac56728298d1658db89a091337e622
Jack Davis <jack@planes.agency>
2024-05-30 10:08:59 +0100

    Add link to app header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@fontsource/fira-mono';
 import type {AppProps} from 'next/app';
+import NextLink from 'next/link';
 import {ChakraProvider, Heading, Box} from '@chakra-ui/react';
 import theme from '../theme';
 import Footer from '@/components/footer';
@@ -8,12 +9,14 @@ export default function App({Component, pageProps}: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Box maxW={600} m="0 auto" px={8} minH="80vh">
-        <Heading as="h1" mt={20} mb={6}>
-          Internal Testing Demonstration
-        </Heading>
+        <NextLink href="/">
+          <Heading as="h1" mt={20} mb={6}>
+            Internal Testing Demonstration
+          </Heading>
+        </NextLink>
         <Component {...pageProps} />
+        <Footer />
       </Box>
-      <Footer />
     </ChakraProvider>
   );
 }
